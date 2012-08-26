b100ede7d6cfde27d31ba61b6ad28c5505b65070
A.Nakano <q9531045@eng.kitakyu-u.ac.jp>
2012-08-26 23:48:10 +0900

    1以外の数字のメソッドを作成

--- OOP_damyCalc/ViewController.m
+++ OOP_damyCalc/ViewController.m
@@ -33,6 +33,10 @@
 }
 
 - (IBAction)num0Button_down:(id)sender {
+    if ([viewLabel.text isEqualToString:@"0"] == YES) {//0の場合何もしない
+    }else{//0でない場合，文字列の右に追加
+        viewLabel.text = [viewLabel.text stringByAppendingString:@"0"];
+    }
 }
 
 - (IBAction)num1Button_down:(id)sender {
@@ -44,27 +48,67 @@
 }
 
 - (IBAction)num2Button_down:(id)sender {
+    if ([viewLabel.text isEqualToString:@"0"] == YES) {//0の場合上書き
+        viewLabel.text = @"2";
+    }else{//0でない場合，文字列の右に追加
+        viewLabel.text = [viewLabel.text stringByAppendingString:@"2"];
+    }
 }
 
 - (IBAction)num3Button_down:(id)sender {
+    if ([viewLabel.text isEqualToString:@"0"] == YES) {//0の場合上書き
+        viewLabel.text = @"3";
+    }else{//0でない場合，文字列の右に追加
+        viewLabel.text = [viewLabel.text stringByAppendingString:@"3"];
+    }
 }
 
 - (IBAction)num4Button_down:(id)sender {
+    if ([viewLabel.text isEqualToString:@"0"] == YES) {//0の場合上書き
+        viewLabel.text = @"4";
+    }else{//0でない場合，文字列の右に追加
+        viewLabel.text = [viewLabel.text stringByAppendingString:@"4"];
+    }
 }
 
 - (IBAction)num5Button_down:(id)sender {
+    if ([viewLabel.text isEqualToString:@"0"] == YES) {//0の場合上書き
+        viewLabel.text = @"5";
+    }else{//0でない場合，文字列の右に追加
+        viewLabel.text = [viewLabel.text stringByAppendingString:@"5"];
+    }
 }
 
 - (IBAction)num6Button_down:(id)sender {
+    if ([viewLabel.text isEqualToString:@"0"] == YES) {//0の場合上書き
+        viewLabel.text = @"6";
+    }else{//0でない場合，文字列の右に追加
+        viewLabel.text = [viewLabel.text stringByAppendingString:@"6"];
+    }
 }
 
 - (IBAction)num7Button_down:(id)sender {
+    if ([viewLabel.text isEqualToString:@"0"] == YES) {//0の場合上書き
+        viewLabel.text = @"7";
+    }else{//0でない場合，文字列の右に追加
+        viewLabel.text = [viewLabel.text stringByAppendingString:@"7"];
+    }
 }
 
 - (IBAction)num8Button_down:(id)sender {
+    if ([viewLabel.text isEqualToString:@"0"] == YES) {//0の場合上書き
+        viewLabel.text = @"8";
+    }else{//0でない場合，文字列の右に追加
+        viewLabel.text = [viewLabel.text stringByAppendingString:@"8"];
+    }
 }
 
 - (IBAction)num9Button_down:(id)sender {
+    if ([viewLabel.text isEqualToString:@"0"] == YES) {//0の場合上書き
+        viewLabel.text = @"9";
+    }else{//0でない場合，文字列の右に追加
+        viewLabel.text = [viewLabel.text stringByAppendingString:@"9"];
+    }
 }
 
 - (IBAction)addButton_down:(id)sender {
