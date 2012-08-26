ec671f40ebb019eac4832b2f288bbe1c24065952
A.Nakano <q9531045@eng.kitakyu-u.ac.jp>
2012-08-26 23:34:51 +0900

    トリガーのメソッドを追加

--- OOP_damyCalc/ViewController.h
+++ OOP_damyCalc/ViewController.h
@@ -8,6 +8,29 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController{
+    __weak IBOutlet UILabel *viewLabel;
+}
+
+- (IBAction)num0Button_down:(id)sender;
+- (IBAction)num1Button_down:(id)sender;
+- (IBAction)num2Button_down:(id)sender;
+- (IBAction)num3Button_down:(id)sender;
+- (IBAction)num4Button_down:(id)sender;
+- (IBAction)num5Button_down:(id)sender;
+- (IBAction)num6Button_down:(id)sender;
+- (IBAction)num7Button_down:(id)sender;
+- (IBAction)num8Button_down:(id)sender;
+- (IBAction)num9Button_down:(id)sender;
+
+- (IBAction)addButton_down:(id)sender;
+- (IBAction)subButton_down:(id)sender;
+- (IBAction)mulButton_down:(id)sender;
+- (IBAction)divButton_down:(id)sender;
+
+- (IBAction)signButton_down:(id)sender;
+- (IBAction)periodButton_down:(id)sender;
+- (IBAction)equalButton_down:(id)sender;
+- (IBAction)ACButton_down:(id)sender;
 
 @end
--- OOP_damyCalc/ViewController.m
+++ OOP_damyCalc/ViewController.m
@@ -22,6 +22,7 @@
 
 - (void)viewDidUnload
 {
+    viewLabel = nil;
     [super viewDidUnload];
     // Release any retained subviews of the main view.
 }
@@ -31,4 +32,57 @@
     return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
 }
 
+- (IBAction)num0Button_down:(id)sender {
+}
+
+- (IBAction)num1Button_down:(id)sender {
+}
+
+- (IBAction)num2Button_down:(id)sender {
+}
+
+- (IBAction)num3Button_down:(id)sender {
+}
+
+- (IBAction)num4Button_down:(id)sender {
+}
+
+- (IBAction)num5Button_down:(id)sender {
+}
+
+- (IBAction)num6Button_down:(id)sender {
+}
+
+- (IBAction)num7Button_down:(id)sender {
+}
+
+- (IBAction)num8Button_down:(id)sender {
+}
+
+- (IBAction)num9Button_down:(id)sender {
+}
+
+- (IBAction)addButton_down:(id)sender {
+}
+
+- (IBAction)subButton_down:(id)sender {
+}
+
+- (IBAction)divButton_down:(id)sender {
+}
+
+- (IBAction)signButton_down:(id)sender {
+}
+
+- (IBAction)periodButton_down:(id)sender {
+}
+
+- (IBAction)mulButton_down:(id)sender {
+}
+
+- (IBAction)equalButton_down:(id)sender {
+}
+
+- (IBAction)ACButton_down:(id)sender {
+}
 @end
